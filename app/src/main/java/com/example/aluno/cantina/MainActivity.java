fb0968c2df32313f24989d34e224c5000bb730a6
dantesparta e otariokk <dantesparta100@gmail.com>
2018-06-25 10:03:56 -0300

    ciracao do projeto..

---
+++ app/src/main/java/com/example/aluno/cantina/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.aluno.cantina;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button botaoProd;
+    Button sobreNos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        botaoProd = (Button) findViewById(R.id.buttonProdutos);
+        sobreNos = (Button) findViewById(R.id.buttonNos);
+
+        botaoProd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, ProdutosActivity.class));
+
+
+
+            }
+        });
+    }
+}
