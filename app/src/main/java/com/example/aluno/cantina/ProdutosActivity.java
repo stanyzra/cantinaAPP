42681c6858516c49d285c80b5c5161ddb0aa5041
stanyzra <dantesparta100@gmail.com>
2018-07-09 10:12:42 -0300

    kkkkkk 09/07/kk

--- app/src/main/java/com/example/aluno/cantina/ProdutosActivity.java
+++ app/src/main/java/com/example/aluno/cantina/ProdutosActivity.java
@@ -2,12 +2,32 @@ package com.example.aluno.cantina;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProdutosActivity extends AppCompatActivity {
+
+    Button botaoMais;
+    Button botaoMenos;
+    TextView qtde;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produtos);
+
+        botaoMais = (Button) findViewById(R.id.buttonMaisSalgado);
+        botaoMais = (Button) findViewById(R.id.buttonMaisRefri);
+        botaoMais = (Button) findViewById(R.id.buttonMaisBalas);
+        botaoMais = (Button) findViewById(R.id.buttonMaisPirulito);
+        botaoMais = (Button) findViewById(R.id.buttonMaisCapu);
+
+        botaoMenos = (Button) findViewById(R.id.buttonMenosSalgado);
+        botaoMenos = (Button) findViewById(R.id.buttonMenosRefri);
+        botaoMenos = (Button) findViewById(R.id.buttonMenosBalas);
+        botaoMenos = (Button) findViewById(R.id.buttonMenosPirulito);
+        botaoMenos = (Button) findViewById(R.id.buttonMenosCapu);
+
+
     }
 }
